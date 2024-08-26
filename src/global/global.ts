3bb2179553e1ffdf85c69e060f1cca567ce4fede
igor_alves <igornalves08@gmail.com>
2024-08-26 16:16:45 -0300

    first commit

---
+++ src/global/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+import { defaultTheme } from "./styles/default";
+
+
+export const GlobalDefault = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: ${defaultTheme.colors.base.gray100};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Inter', sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+    }
+`
---
+++ src/global/styles/default.ts
@@ -0,0 +1,37 @@
+export const defaultTheme = {
+    colors: {
+        principal: {
+            purpleLight: '#DDD2EF',
+            purpleBase: '#9359F3',
+            purpleDark: '#6F3CC3',
+            greenLight: '#BFE3D0',
+            greenBase: '#479C6E',
+            greenDark: '#2D6C4A'
+        },
+
+        base: {
+            gray100: '#F0EDF2',
+            gray200: '#E5E2E9',
+            gray330: '#E0DCE4',
+            gray400: '#D1CBD7',
+            gray500: '#6B6572',
+            gray600: '#262428'
+        }, 
+
+        feedback: {
+            danger: '#C2464D'
+        }
+    },
+
+    fontWeight: {
+        Regular: 400,
+        SemiBold: 600,
+        Bold: 700
+    }, 
+     
+    fontSizes: {
+        xs: 12,
+        sm: 14,
+        md: 16,
+    },
+}
